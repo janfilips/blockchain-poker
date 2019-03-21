176434de2b965d2d97be03cb195922c103b6efc3
Jan Philips <janphilips@Jan-MacBook-Pro.local>
2019-03-21 10:05:54 +0100

    added on staticfiles config

--- game/settings.py
+++ game/settings.py
@@ -119,4 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'static'))
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.abspath(os.path.join(BASE_DIR, 'static')),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
