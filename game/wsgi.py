e473769c690019afdf0b416375af7155794b0c4a
Jan Philips <janphilips@Jan-MacBook-Pro.local>
2019-03-21 09:33:28 +0100

    adjusted naming convention

--- game/wsgi.py
+++ game/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for poker project.
+WSGI config for the poker project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game.settings')
 
 application = get_wsgi_application()
