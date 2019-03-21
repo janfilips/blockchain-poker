28b0eec36d56f4ff651b896942009ca789a76f73
Jan Philips <janphilips@Jan-MacBook-Pro.local>
2019-03-21 10:36:15 +0100

    made the about page to work

--- game/urls.py
+++ game/urls.py
@@ -4,11 +4,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
-from game.views import home
+from game.views import home, about
 
 urlpatterns = [
     # common views
     url(r'^$', home, name='home'),
+    url(r'^about$', about, name='about'),
     # ajax
-    #url(r'^ajax/bet/$', ajax_bet, name='ajax_bet'),
+    #url(r'^ajax/bet$', ajax_bet, name='ajax_bet'),
 ]
