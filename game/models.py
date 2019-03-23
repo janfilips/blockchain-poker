42a680a81e370499a02faff0df1e4ba6f3f48438
Jan Philips <janphilips@Jan-MacBook-Pro.local>
2019-03-24 00:28:36 +0100

    defined basic models

---
+++ game/models.py
@@ -0,0 +1,18 @@
+import os
+
+from django.conf import settings
+from django.db import models
+
+class Players(models.Model):
+    address = models.CharField(max_length=128, default="")
+    session_key = models.CharField(max_length=128)
+
+class Decks(models.Model):
+    player = models.ForeignKey(Players, on_delete=models.CASCADE)
+    deck_id = models.AutoField(primary_key=True)
+    deck = models.CharField(max_length=1024)
+
+class Wins(models.Model):
+    player = models.ForeignKey(Players, on_delete=models.CASCADE)
+    deck = models.ForeignKey(Decks, on_delete=models.CASCADE)
+    winning_hand = models.CharField(max_length=64)
