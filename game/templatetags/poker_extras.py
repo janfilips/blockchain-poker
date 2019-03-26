1105c1c9ce1748f1a1cc11364b8e31c54a875eba
Jan Philips <janphilips@Jan-MacBook-Pro.local>
2019-03-26 01:28:24 +0100

    applied tx shrink str filter

--- game/templatetags/poker_extras.py
+++ game/templatetags/poker_extras.py
@@ -21,3 +21,7 @@ def random_int(a, b=None):
     if b is None:
         a, b = 0, a
     return random.randint(a, b)
+
+@register.filter(name="shrink_tx_string")
+def shrink_tx_string(str):
+    return "XXX working on this currently"
