c680c4b9cf08cd0198ab6f71fc35a7afad42e5d9
Jan Philips <janphilips@Jan-MacBook-Pro.local>
2019-03-26 01:19:54 +0100

    drafted out basic filters on the templatetags

--- game/templatetags/poker_extras.py
+++ game/templatetags/poker_extras.py
@@ -2,3 +2,20 @@ import os
 import random
 import datetime
 import json
+
+register = template.Library()
+
+
+@register.filter(name='dict_get')
+def dict_get(h, key):
+    try:
+        return h[key]
+    except:
+        return None
+
+@register.filter(name="random_int")
+def random_int(a, b=None):
+    if b is None:
+        a, b = 0, a
+    return random.randint(a, b)
+
