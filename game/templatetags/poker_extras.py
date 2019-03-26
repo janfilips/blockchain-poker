c5eebab236a0418deb3f85b7ad96395418f7b708
Jan Philips <janphilips@Jan-MacBook-Pro.local>
2019-03-26 01:35:37 +0100

    cleared unused code

--- game/templatetags/poker_extras.py
+++ game/templatetags/poker_extras.py
@@ -21,7 +21,3 @@ def random_int(a, b=None):
     if b is None:
         a, b = 0, a
     return random.randint(a, b)
-
-@register.filter(name="shrink_tx_string")
-def shrink_tx_string(str):
-    return "XXX working on this currently"
