c58b3b61fef8124ea8b59ab8e2705130f02cd96e
Jan Philips <janphilips@Jan-MacBook-Pro.local>
2019-03-31 03:11:58 +0200

    adjusted simulated credit

--- game/views.py
+++ game/views.py
@@ -64,9 +64,9 @@ def home(request):
     #########################################################################
     # XXX temporarily simulating credit
     if(player.credit == 0):
-        player.credit = 6
+        player.credit = 30
         player.save()
-    player.credit -= 1
+    player.credit -= 5
     player.save()
     #########################################################################
 
