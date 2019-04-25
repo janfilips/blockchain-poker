464457556f24a7f7202168fac683fd03055fdbbe
Jan Filips <jan.filips@segron.com>
2019-04-25 18:36:03 +0200

    sc

--- game/views.py
+++ game/views.py
@@ -48,30 +48,43 @@ def home(request):
     # hand.insert(0, card4)
     # hand.insert(0, card5)
 
-    hand = cards_deck.get_hand()
-    evaluated_hand, numeral_dict, suit_dict = cards_deck.evaluate_hand(hand)
-    sugested_hand = cards_deck.suggest_hand(player, hand, evaluated_hand, numeral_dict, suit_dict)
-
-    print('evaluated_hand debug', evaluated_hand, numeral_dict, suit_dict)
-    print('suggested_hand debug', sugested_hand)
-
-    deck_hash = (''.join([choice(string.ascii_letters + string.digits) for i in range(25)]) + \
-                        ''.join([choice(string.digits) for i in range(10)])).upper()
-
-    starting_nonreduced_cards_deck_ = ""
-    for card in starting_nonreduced_cards_deck:
-        starting_nonreduced_cards_deck_ += str(card) + "|"
-    starting_cards_deck = starting_nonreduced_cards_deck_[:-1]
-
-    player_cards_deck = Decks.objects.create(player=player, bet_amount=player.bet_amount, deck=starting_cards_deck, deck_hash=deck_hash)
-    print('player_cards_deck', player_cards_deck)
-
     #########################################################################
     # XXX temporarily simulating credit
     if(player.credit <= 0):
-        player.credit = 5
+        player.credit = 10
         player.save()
     #########################################################################
+
+
+    if(player.credit >= player.bet_amount):
+
+        hand = cards_deck.get_hand()
+        evaluated_hand, numeral_dict, suit_dict = cards_deck.evaluate_hand(hand)
+        sugested_hand = cards_deck.suggest_hand(player, hand, evaluated_hand, numeral_dict, suit_dict)
+
+        print('evaluated_hand debug', evaluated_hand, numeral_dict, suit_dict)
+        print('suggested_hand debug', sugested_hand)
+
+        deck_hash = (''.join([choice(string.ascii_letters + string.digits) for i in range(25)]) + \
+                            ''.join([choice(string.digits) for i in range(10)])).upper()
+
+        starting_nonreduced_cards_deck_ = ""
+        for card in starting_nonreduced_cards_deck:
+            starting_nonreduced_cards_deck_ += str(card) + "|"
+        starting_cards_deck = starting_nonreduced_cards_deck_[:-1]
+
+        player_cards_deck = Decks.objects.create(player=player, bet_amount=player.bet_amount, deck=starting_cards_deck, deck_hash=deck_hash)
+        print('player_cards_deck', player_cards_deck)
+
+        player.credit -= player.bet_amount
+        player.save()
+
+    else:
+
+        hand = []
+        evaluated_hand = ""
+        sugested_hand = ""
+
 
     winning_decks = Decks.objects.filter(player_wins=True).order_by('-pk')[:100]
 
@@ -80,14 +93,11 @@ def home(request):
         player.swap_bet_amount = 0
         player.save()
 
+
     autoplay = "false"
     if(player.autoplay):
         autoplay = "true"
-
-    autoplay = "true"
-
-    player.credit -= player.bet_amount
-    player.save()
+    #autoplay = "true"
 
     response = render(
         request=request,
