5160b994bb3343334b433d6cc9fe774afc7bb76f
Jan Filips <jan.filips@segron.com>
2019-05-07 22:02:42 +0200

    started on main function on the discriminator

--- utils/discriminator.py
+++ utils/discriminator.py
@@ -4,3 +4,16 @@ import itertools
 import random
 
 # XXX TODO deck discriminator
+
+def discriminate(hand, deck):
+    return "XXX TODO"
+
+
+if __name__ == '__main__':
+
+    print('deck discriminator')
+    print('XXX TODO working on this currently..')
+
+    hand = deck().get_hand()
+    evaluated_hand = deck().evaluate_hand(hand)
+    print(hand, evaluated_hand[0])
