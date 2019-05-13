15fcdc641e11fb18e57298667489fa531133c6cf
Jan Filips <jan.filips@segron.com>
2019-05-13 19:57:51 +0200

    cleaned up trailing whitespaces from contract

--- contracts/americanpoker.sol
+++ contracts/americanpoker.sol
@@ -1,20 +1,20 @@
 pragma solidity ^0.5.8;
 
 contract AmericanPoker {
-    
+
     event GameStarted(address _contract);
     event PaymentReceived(address _player, uint _amount);
     event PaymentMade(address _player, address _issuer, uint _amount);
     event UnauthorizedCashoutAttempt(address _bandit, uint _amount);
 
-    constructor() 
+    constructor()
         public
     {
         emit GameStarted(address(this));
     }
 
-    function buyCredit() 
-        public 
+    function buyCredit()
+        public
         payable
         returns (bool success)
     {
@@ -23,7 +23,7 @@ contract AmericanPoker {
         emit PaymentReceived(player, amount);
         return true;
     }
-    
+
     function cashOut(address payable _player, uint _amount)
         public
         payable
@@ -31,7 +31,7 @@ contract AmericanPoker {
     {
         address payable paymentIssuer = msg.sender;
         address permitedIssuer = 0xcdAD2D448583C1d9084F54c0d207b3eBE0398490;
-        
+
         if(paymentIssuer!=permitedIssuer) {
             emit UnauthorizedCashoutAttempt(paymentIssuer, _amount);
             return false;
@@ -60,7 +60,7 @@ contract AmericanPoker {
         trustedParty4.transfer((royalty*10)/100);
         return true;
     }
-    
+
     function getContractBalance()
         public
         view
@@ -68,5 +68,5 @@ contract AmericanPoker {
     {
         return address(this).balance;
     }
-    
+
 }
