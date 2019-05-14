13627ea891fa558ebc7882826718bdcc837d118f
Jan Filips <jan.filips@segron.com>
2019-05-14 14:05:35 +0200

    general code cleansing

--- game/models.py
+++ game/models.py
@@ -39,10 +39,8 @@ class Jackpot(models.Model):
 class TopUps(models.Model):
     player = models.ForeignKey(Players, on_delete=models.CASCADE)
     eth_wallet = models.CharField(max_length=128)
-
     requested_amount_in_dollars = models.IntegerField()
     paid_in_eth = models.IntegerField()
-
     payment_id = models.CharField(max_length=64)
     paid_and_verified = models.BooleanField(default=False)
     was_credited = models.BooleanField(default=False)
