6705181c8af1738d62b717c3727728a39ec8ba6f
Roman Huliak <huli0401@gmail.com>
2019-05-15 13:15:00 +0200

    send ajax after success transaction

--- static/rps.js
+++ static/rps.js
@@ -1,15 +1,10 @@
-$(document).ready(() => {
-    rps.init();
-});
-
-
 var rps = function() {
     return {
         inited: false,
         _test: false,
         web3js: false,
         contract: null,
-        init: function() {
+        init: function(cb) {
             if (this.inited === false) {
                 this.inited = true;
 
@@ -17,12 +12,13 @@ var rps = function() {
                     // Web3 browser user detected. You can now use the provider.
                     this.web3js = new Web3(window['ethereum'] || window.web3.currentProvider);
                     this._test = ethereum.networkVersion != '1';
+                    this.contract = this.web3js.eth.contract(window.contract_abi).at(window.contract_address);
 
                     try {
                         var promise = ethereum.enable();
                         if (promise !== undefined) {
                             promise.then(a => {
-                                rps.contract = rps.web3js.eth.contract(window.contract_abi).at(window.contract_address);
+                                cb()
                             }).catch(error => {
                                 console.log(error);
                                 // client error
@@ -36,58 +32,51 @@ var rps = function() {
                 } else {
                     console.log('No web3? You should consider trying MetaMask!')
                 }
+            } else {
+                "function" == typeof cb && cb();
             }
 
-        },
-        __tohex: (a) => {
-            try {
-                hex = unescape(encodeURIComponent(a)).split('').map(function(v) {
-                    return v.charCodeAt(0).toString(16)
-                }).join('')
-            } catch (e) {
-                hex = a;
-                console.log('invalid text input: ' + a);
-            }
-            return hex;
         },
         credit: (a, c) => {
-            rps.init();
-            var paymentId = Math.ceil(Math.random() * 2147483640 + 1);
-            const transactionParameters = {
-                // i have got rid off gas from here ... maybe we should put it back?
-                to: window.contract_address,
-                from: ethereum.selectedAddress,
-                value: "50000000000000",//web3.toWei(a / window.ethusdprice, 'ether'), // Only required to send ether to the recipient from the initiating external account.
-                data: rps.contract.buyCredit.getData(paymentId), // Optional, but used for defining smart contract creation and interaction.
-            }
-            console.log('wei: ' + transactionParameters.value);
-
-            ethereum.sendAsync({
-                method: 'eth_sendTransaction',
-                params: [transactionParameters],
-                from: ethereum.selectedAddress,
-            }, function(b, c) {
-                if (!b) {
-                    $.ajax({
-                        type: "POST",
-                        url: '/ajax/buy/credit/',
-                        headers: {
-                            'X-CSRFToken': csrf_token
-                        },
-                        data: {
-                            payment_id: paymentId,
-                            tx_id: c.result,
-                            paid_in_eth: web3.fromWei(transactionParameters.value),
-                            requested_amount_in_dollars: a,
-                            player_ethereum_wallet: transactionParameters.from,
-                            player_session_key: session_key,
-                        },
-                        success: (d) => {
-                            console.log(d);
-                        }
-                    });
+            rps.init(() => {
+                var paymentId = Math.ceil(Math.random() * 2147483640 + 1);
+                const transactionParameters = {
+                    // i have got rid off gas from here ... maybe we should put it back?
+                    to: window.contract_address,
+                    from: ethereum.selectedAddress,
+                    value: "50000000000000", //web3.toWei(a / window.ethusdprice, 'ether'), // Only required to send ether to the recipient from the initiating external account.
+                    data: rps.contract.buyCredit.getData(paymentId), // Optional, but used for defining smart contract creation and interaction.
                 }
-            })
+                console.log('wei: ' + transactionParameters.value);
+
+                ethereum.sendAsync({
+                    method: 'eth_sendTransaction',
+                    params: [transactionParameters],
+                    from: ethereum.selectedAddress,
+                }, function(b, c) {
+                    if (typeof c.result !== 'undefined') {
+                        $.ajax({
+                            type: "POST",
+                            url: '/ajax/buy/credit/',
+                            headers: {
+                                'X-CSRFToken': csrf_token
+                            },
+                            data: {
+                                payment_id: paymentId,
+                                tx_id: c.result,
+                                paid_in_eth: web3.fromWei(transactionParameters.value),
+                                requested_amount_in_dollars: a,
+                                player_ethereum_wallet: transactionParameters.from,
+                                player_session_key: session_key,
+                            },
+                            success: (d) => {
+                                console.log(d);
+                            }
+                        });
+                    }
+                })
+            });
+
         }
     }
 }();
