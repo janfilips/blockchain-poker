d0c03551fcd1acfe4504ef502d943a71465d9e79
Roman Huliak <huli0401@gmail.com>
2019-05-15 15:15:04 +0200

    payment verify, wait, redirect, metamask errors

--- static/rps.js
+++ static/rps.js
@@ -27,63 +27,77 @@ var rps = function() {
                         var promise = ethereum.enable();
                         if (promise !== undefined) {
                             promise.then(a => {
-                                cb()
+                                if (typeof ethereum.selectedAddress === 'undefined') {
+                                    $('#popup3').show();
+                                } else {
+                                    cb()
+                                }
                             }).catch(error => {
                                 console.log(error);
-                                // client error
+                                $('#popup2').show();
                             });
                         }
                     } catch (e) {
-                        // private network
+                        $('#popup2').show();
                     }
-
-                    // console.log(error);
                 } else {
-                    console.log('No web3? You should consider trying MetaMask!')
+                    $('#popup1').show();
                 }
             } else {
-                "function" == typeof cb && cb();
+                if (typeof ethereum.selectedAddress === 'undefined') {
+                    $('#popup3').show();
+                } else {
+                    "function" == typeof cb && cb();
+                }
             }
 
         },
         credit: (a, c) => {
             rps.init(() => {
-                var paymentId = rps.b32(Math.ceil(Math.random() * 2147483640 + 1));
-                const transactionParameters = {
-                    // i have got rid off gas from here ... maybe we should put it back?
-                    to: window.contract_address,
-                    from: ethereum.selectedAddress,
-                    value: web3.toHex(web3.toWei(a / window.ethusdprice, 'ether')), // Only required to send ether to the recipient from the initiating external account.
-                    data: rps.contract.buyCredit.getData(paymentId), // Optional, but used for defining smart contract creation and interaction.
-                }
-                console.log('wei: ' + transactionParameters.value);
-
-                ethereum.sendAsync({
-                    method: 'eth_sendTransaction',
-                    params: [transactionParameters],
-                    from: ethereum.selectedAddress,
-                }, function(b, c) {
-                    if (typeof c.result !== 'undefined') {
-                        $.ajax({
-                            type: "POST",
-                            url: '/ajax/buy/credit/',
-                            headers: {
-                                'X-CSRFToken': csrf_token
-                            },
-                            data: {
-                                payment_id: paymentId,
-                                tx_id: c.result,
-                                paid_in_eth: a / window.ethusdprice,
-                                requested_amount_in_dollars: a,
-                                player_ethereum_wallet: transactionParameters.from,
-                                player_session_key: session_key,
-                            },
-                            success: (d) => {
-                                console.log(d);
-                            }
-                        });
+                // @todo on production remove OR condition
+                if (rps._test === false || true) {
+                    var paymentId = rps.b32(Math.ceil(Math.random() * 2147483640 + 1));
+                    const transactionParameters = {
+                        // i have got rid off gas from here ... maybe we should put it back?
+                        to: window.contract_address,
+                        from: ethereum.selectedAddress,
+                        value: web3.toHex(web3.toWei(a / window.ethusdprice, 'ether')), // Only required to send ether to the recipient from the initiating external account.
+                        data: rps.contract.buyCredit.getData(paymentId), // Optional, but used for defining smart contract creation and interaction.
                     }
-                })
+                    console.log('wei: ' + transactionParameters.value);
+
+                    ethereum.sendAsync({
+                        method: 'eth_sendTransaction',
+                        params: [transactionParameters],
+                        from: ethereum.selectedAddress,
+                    }, function(b, c) {
+                        if (typeof c.result !== 'undefined') {
+                            $.ajax({
+                                type: "POST",
+                                url: '/ajax/buy/credit/',
+                                headers: {
+                                    'X-CSRFToken': csrf_token
+                                },
+                                data: {
+                                    payment_id: paymentId,
+                                    tx_id: c.result,
+                                    paid_in_eth: a / window.ethusdprice,
+                                    requested_amount_in_dollars: a,
+                                    player_ethereum_wallet: transactionParameters.from,
+                                    player_session_key: session_key,
+                                },
+                                success: (d) => {
+                                    console.log(d);
+                                    if (d === 'True') {
+                                        window.location.href = '/payment/verifying#' + paymentId;
+                                    }
+                                }
+                            });
+                        }
+                    })
+                } else {
+                    $('#popup2').show();
+                }
             });
 
         }
