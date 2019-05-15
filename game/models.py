0cd965e24b437f20916128ccb451a5c670d44f7e
Jan Filips <jan.filips@segron.com>
2019-05-15 13:27:29 +0200

    increased length for the tx_id data type

--- game/models.py
+++ game/models.py
@@ -41,7 +41,7 @@ class TopUps(models.Model):
     eth_wallet = models.CharField(max_length=128)
     requested_amount_in_dollars = models.IntegerField()
     paid_in_eth = models.FloatField()
-    payment_id = models.CharField(max_length=64)
+    payment_id = models.CharField(max_length=128)
     tx_id = models.CharField(max_length=128)
     paid_and_verified = models.BooleanField(default=False)
     verification_attempts = models.IntegerField(default=0)
