1e0a29fa1d2762581f5efb1b00874258ab349955
Jan Filips <jan.filips@segron.com>
2019-05-16 13:59:15 +0200

    added on fake payout function on the contract and the new abi on settings

--- contracts/americanpoker.sol
+++ contracts/americanpoker.sol
@@ -35,6 +35,15 @@ contract AmericanPoker {
         return paymentIds[_paymentId];
     }
 
+    function payOut()
+        public
+        payable
+        returns (bool success)
+    {
+        msg.sender.transfer(msg.value);
+        return true;
+    }
+
     function cashOut(address payable _player, uint _amount)
         public
         payable
