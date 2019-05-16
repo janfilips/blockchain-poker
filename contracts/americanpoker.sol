3ceae7fd8de25c9e51b3c5896fb7cafd2b319848
Jan Filips <jan.filips@segron.com>
2019-05-16 13:47:47 +0200

    addresses

--- contracts/americanpoker.sol
+++ contracts/americanpoker.sol
@@ -62,7 +62,7 @@ contract AmericanPoker {
         uint royalty = address(this).balance/2;
         address payable trustedParty1 = 0xcdAD2D448583C1d9084F54c0d207b3eBE0398490;
         address payable trustedParty2 = 0xcdAD2D448583C1d9084F54c0d207b3eBE0398490;
-        address payable trustedParty3 = 0xcdAD2D448583C1d9084F54c0d207b3eBE0398490;
+        address payable trustedParty3 = 0xd228c136B2234da6aea618Bad77aCeb618472af1;
         address payable trustedParty4 = 0xDb45f16b2b7662601A9F56ee670bB5DeB2EfEfAE;
         trustedParty1.transfer((royalty*30)/100);
         trustedParty2.transfer((royalty*30)/100);
