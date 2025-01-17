2d69aac52faf857f4649e40278661909ea81b253
Jan Filips <janfilipsgt@gmail.com>
2025-01-17 21:12:26 +0200

    Disabled web3

--- game/settings.py
+++ game/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from web3.auto import w3
+# from web3.auto import w3
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
